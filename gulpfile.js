ae44bba0a1abf06f1d09e787fd1a783f600cced7
Bartosz Czerwiński <bartosz@czerwinb.pl>
2018-01-25 23:48:20 +0100

    Define Gulp build for JiraStatusResolver module

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,11 @@ let buffer = require('vinyl-buffer');
 let browserify = require('browserify');
 let babelify = require('babelify');
 
-function streamBrowserify() {
+function streamBrowserify(moduleName, moduleFilename) {
     return browserify({
-        'entries': ['src/main/javascript/metrics-calculator.js'],
+        'entries': ['src/main/javascript/' + moduleFilename],
         'debug': true,
+        'standalone': moduleName,
         'transform': [
             babelify.configure({
                 'presets': ['es2015']
@@ -17,19 +18,23 @@ function streamBrowserify() {
     });
 }
 
-function build() {
-    return streamBrowserify()
-        .bundle()
-        .on('error', function (e) {
-            console.error(e);
-            this.emit('end');
-        })
-        .pipe(source('metrics-calculator-bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('target/classes/js'));
+function bundleJSModule(moduleName, moduleFilename) {
+    return () => {
+        streamBrowserify(moduleName, moduleFilename)
+            .bundle()
+            .on('error', function (e) {
+                console.error(e);
+                this.emit('end');
+            })
+            .pipe(source(moduleFilename.replace('.js', '.bundle.js')))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('target/classes/js'));
+    };
 }
 
-gulp.task('build', build);
-gulp.task('default', ['build']);
+gulp.task('bundle-MetricsCalculator', bundleJSModule('MetricsCalculator', 'metrics-calculator.js'));
+gulp.task('bundle-JiraStatusResolver', bundleJSModule('JiraStatusResolver', 'jira-status-resolver.js'));
+
+gulp.task('default', ['bundle-MetricsCalculator', 'bundle-JiraStatusResolver']);
